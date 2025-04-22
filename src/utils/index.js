86bcdc73c9cc3f9dd0eaf63211b005877d992ee0
lenjstlp <lenjstlp@gmail.com>
2025-04-22 23:40:31 +0800

    feat: 新增字典取值公共方法

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,8 @@
 export { getToken, setToken, removeToken } from './token'
+
+// 字典label选取
+export function dictSelect(arr, value) {
+    return arr.find(i => {
+        return i.value === value
+    })?.label || value
+}
