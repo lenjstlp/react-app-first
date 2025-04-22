d36b1a8428f6e5c68629e6291f6f6742a020de25
lenjstlp <lenjstlp@gmail.com>
2025-04-22 23:43:31 +0800

    feat: 测试接口401

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from 'react'
+import http from '@/http'
+
 function Home() {
+
+    useEffect(() => {
+        async function getLogout401() {
+            const res = await http.get('/logout401')
+            console.log(res, '===---');
+        }
+        // getLogout401()
+    }, [])
+
     return (
         <div>Home</div>
     )
