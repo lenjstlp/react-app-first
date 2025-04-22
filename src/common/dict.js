c7fa4a00aaa91bd9dda5368dcdee10fc9291133f
lenjstlp <lenjstlp@gmail.com>
2025-04-22 23:38:18 +0800

    feat: 公共字典数据前端暂时固定

---
+++ src/common/dict.js
@@ -0,0 +1,19 @@
+// vip level
+export const vitOptions = [
+    {
+        value: '1',
+        label: '至尊会员'
+    },
+    {
+        value: '2',
+        label: '超级会员'
+    },
+    {
+        value: '3',
+        label: '会员'
+    },
+    {
+        value: '5',
+        label: '大众'
+    },
+]
