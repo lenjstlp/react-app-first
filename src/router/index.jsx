0ded302b79394fc54472045f34b3d17bcdc8774f
lenjstlp <lenjstlp@gmail.com>
2025-04-22 14:53:10 +0800

    feat: token做路由权限控制

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,11 +4,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/pages/Layout/index.jsx";
 import Login from "@/pages/Login/index";
 
+import { AuthRoute } from '@/components/AuthRoute'
+
 // 配置路由实例
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Layout />
+        element: <AuthRoute><Layout /></AuthRoute>
     },
     {
         path: '/login',
