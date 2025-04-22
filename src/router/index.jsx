f01c5109516b3d1be911b85524ab31a8c3ae1518
lenjstlp <lenjstlp@gmail.com>
2025-04-22 17:57:13 +0800

    feat: 二级路由配置&二级路由组件创建

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,6 +3,9 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Layout from "@/pages/Layout/index.jsx";
 import Login from "@/pages/Login/index";
+import Home from "@/pages/Home"
+import Article from "@/pages/Article"
+import Publish from "@/pages/Publish"
 
 import { AuthRoute } from '@/components/AuthRoute'
 
@@ -10,7 +13,22 @@ import { AuthRoute } from '@/components/AuthRoute'
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <AuthRoute><Layout /></AuthRoute>
+        element: <AuthRoute><Layout /></AuthRoute>,
+        children: [
+            {
+                // path: '/home',
+                index: true,
+                element: <Home />
+            },
+            {
+                path: '/article',
+                element: <Article />
+            },
+            {
+                path: '/publish',
+                element: <Publish />
+            },
+        ]
     },
     {
         path: '/login',
