e2d91774b439c4d548e8b8a408e0ad76d6a20c69
lenjstlp <lenjstlp@gmail.com>
2025-04-23 16:01:10 +0800

    feat: 新增notfound组件

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom'
+import { Button, Result } from 'antd'
+
+function NotFound() {
+    const navigate = useNavigate()
+
+    function backClick() {
+        navigate('/')
+    }
+    return (
+        <Result
+            status='404'
+            title='404'
+            subTitle='对不起，没有找到您访问的页面。'
+            extra={
+                <Button type='primary' onClick={backClick}>返回登录页</Button>
+            }
+        />
+    )
+}
+
+export default NotFound
