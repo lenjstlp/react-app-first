478807425b1eaffcc7fe1876691048e0bfb737d2
lenjstlp <lenjstlp@gmail.com>
2025-04-23 19:00:50 +0800

    feat: 引入打包体积分析插件

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+import { visualizer } from 'rollup-plugin-visualizer'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(), 
+    visualizer()
+  ],
   resolve: {
     alias: [
       {
