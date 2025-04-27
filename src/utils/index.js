122dc7d45086ae25b94951dc5a9c8ee034016fed
lenjstlp <lenjstlp@gmail.com>
2025-04-27 18:16:58 +0800

    feat: 时间工具封装

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,14 @@
 export { getToken, setToken, removeToken } from './token'
 export { lazyLoad } from './lazyLoad'
+import dayjs from 'dayjs'
 
 // 字典label选取
 export function dictSelect(arr, value) {
     return arr.find(i => {
         return i.value === value
     })?.label || value
+}
+// 时间快捷方法
+export function formatDate(val, date = 'YYYY-MM-DD HH:mm:ss') {
+    return dayjs(val).format(date)
 }
