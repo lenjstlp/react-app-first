541d2087cb6356fc4d282b7f3091c9cfe8bf0e2c
lenjstlp <lenjstlp@gmail.com>
2025-04-30 00:21:14 +0800

    feat: byteMD集成

--- src/pages/CodeResearch/index.jsx
+++ src/pages/CodeResearch/index.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import { Button } from 'antd'
+import { Viewer } from '@bytemd/react'
+
 function CodeResearch() {
   const navigate = useNavigate()
 
@@ -7,6 +9,7 @@ function CodeResearch() {
     <div className='flex justify-center h-[100%]'>
       <div>左侧导航</div>
       <div className='w-[800px] p-[15px] rounded-[10px] h-[100%] bg-[#fff]'>
+        <Viewer />
         <Button onClick={() => navigate('/write')}>写文章</Button>
       </div>
       <div>右侧导航</div>
