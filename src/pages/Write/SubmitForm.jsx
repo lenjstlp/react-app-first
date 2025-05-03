0cc874daf655c1359329a2496dd0c0658c50b67e
lenjstlp <lenjstlp@gmail.com>
2025-05-03 14:06:12 +0800

    feat: byteMD书写页面

---
+++ src/pages/Write/SubmitForm.jsx
@@ -0,0 +1,145 @@
+import { useState, useEffect } from 'react'
+import {
+  Form,
+  Select,
+  Upload,
+  Button,
+  message,
+  Image,
+  Space,
+  Input
+} from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import useDicts from '@/hooks/useDicts'
+import { addAndEditArticle } from '@/apis/article'
+
+function SubmitForm({ articleParams, setPopoverShow }) {
+  // form实例绑定
+  const [form] = Form.useForm()
+
+  // 频道字典获取
+  const { dicts } = useDicts({ type: 'ARTICLE_CHANNEL' })
+
+  console.log(articleParams, 'articleParams')
+
+  useEffect(() => {
+    form.setFieldValue({ ...articleParams })
+  }, [])
+
+  async function onFinish(val) {
+    const formData = new FormData()
+    fileList.forEach((file) => {
+      formData.append('cover', file.originFileObj)
+    })
+    const obj = {
+      ...val,
+      content: articleParams.content,
+      title: articleParams.title,
+      id: articleParams.id
+    }
+    Object.keys(obj).forEach((i) => {
+      formData.append(i, obj[i])
+    })
+    console.log(formData, val, articleParams, 'formData')
+
+    const { code } = await addAndEditArticle(formData, { id: articleParams.id })
+    if (code === 0) {
+      form.resetFields()
+      setPopoverShow(false)
+      message.success('创建文章成功')
+    }
+  }
+
+  // 封面图片上传
+  const [previewOpen, setPreviewOpen] = useState(false)
+  const [previewImage, setPreviewImage] = useState('')
+  const [fileList, setFileList] = useState([])
+  function handleChange(val) {
+    setFileList(val.fileList)
+  }
+  function handlePreview(file) {
+    setPreviewImage(file.url || file.thumbUrl)
+    setPreviewOpen(true)
+  }
+  const uploadButton = (
+    <button style={{ border: 0, background: 'none' }} type='button'>
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </button>
+  )
+  return (
+    <Form
+      form={form}
+      name='createArticle'
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 17 }}
+      layout='horizontal'
+      style={{ maxWidth: 600 }}
+      initialValues={{
+        channel: '1',
+        abstract: '',
+        ...articleParams
+      }}
+      onFinish={onFinish}>
+      <Form.Item
+        label='频道'
+        name='channel'
+        rules={[{ required: true, message: '请输入频道' }]}>
+        <Select
+          placeholder='请选择文章频道'
+          rules={[{ required: true, message: '请选择你的频道' }]}>
+          {dicts['ARTICLE_CHANNEL'] &&
+            dicts['ARTICLE_CHANNEL'].map((i) => {
+              return (
+                <Select.Option value={i.value} key={i.value}>
+                  {i.label}
+                </Select.Option>
+              )
+            })}
+        </Select>
+      </Form.Item>
+      <Form.Item label={null} name='pic'>
+        <Upload
+          listType='picture-card'
+          maxCount={1}
+          onChange={handleChange}
+          onPreview={handlePreview}
+          beforeUpload={() => false}>
+          {uploadButton}
+        </Upload>
+        {previewImage && (
+          <Image
+            wrapperStyle={{ display: 'none' }}
+            preview={{
+              visible: previewOpen,
+              onVisibleChange: (visible) => setPreviewOpen(visible),
+              afterOpenChange: (visible) => !visible && setPreviewImage('')
+            }}
+            src={previewImage}
+          />
+        )}
+      </Form.Item>
+      <Form.Item
+        label='文章摘要'
+        name='abstract'
+        rules={[{ required: true, message: '请输入文章摘要' }]}>
+        <Input.TextArea
+          placeholder='请输入文章摘要'
+          showCount
+          maxLength={100}
+          rows={4}
+        />
+      </Form.Item>
+      <Form.Item style={{ width: '100%' }} label={null}>
+        <Space className='flex justify-end w-[100%]'>
+          <Button onClick={() => setPopoverShow(false)}>取消</Button>
+          <Button type='primary' htmlType='submit'>
+            {articleParams.id ? '确定修改' : '确定创建'}
+          </Button>
+        </Space>
+      </Form.Item>
+    </Form>
+  )
+}
+
+export default SubmitForm
--- src/pages/Write/index.jsx
+++ src/pages/Write/index.jsx
@@ -1,14 +1,98 @@
+import { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { Input, Button, Popover } from 'antd'
 import Editor from '@/components/writeEditor'
+import AvatarPopover from '@/pages/Layout/avatarPopover'
+import SubmitForm from './SubmitForm'
+import { getArticleById } from '@/apis/article'
+
 function Write() {
-  function valueChange(val) {
-    console.log('valueChange', val)
+  const [params] = useSearchParams()
+  const id = params.get('id')
+  async function queryArticleById() {
+    const { code, data } = await getArticleById({ id })
+    if (code === 0) {
+      console.log(data, '===')
+      setAddArticleParams({
+        ...addArticleParams,
+        ...data
+      })
+    }
   }
+
+  useEffect(() => {
+    id && queryArticleById()
+  }, [])
+
+  const [addArticleParams, setAddArticleParams] = useState({
+    title: '',
+    content: '',
+    abstract: ''
+  })
+
+  const [popoverShow, setPopoverShow] = useState(false)
+  const releaseContent = (
+    <div className='w-[390px]'>
+      <div className='text-[20px] pb-[10px] border-b-1 border-[#e3e5e7]'>
+        发布文章
+      </div>
+      <div className='my-[10px]'>
+        <SubmitForm
+          articleParams={addArticleParams}
+          setPopoverShow={setPopoverShow}
+        />
+      </div>
+    </div>
+  )
 
   return (
     <>
-      <div className='h-[60px] bg-[#fff]'>q123</div>
+      <div className='flex items-center justify-between h-[60px] bg-[#fff]'>
+        <Input
+          className='h-[60px]'
+          style={{
+            width: 700,
+            fontSize: '26px'
+          }}
+          placeholder='请输入标题'
+          value={addArticleParams.title}
+          variant='borderless'
+          onChange={(e) => {
+            setAddArticleParams({
+              ...addArticleParams,
+              title: e.target.value
+            })
+          }}
+        />
+        <div className='flex ml-auto mr-[20px]'>
+          <Popover
+            open={popoverShow}
+            placement='bottomRight'
+            content={releaseContent}
+            trigger='click'>
+            <Button
+              type='primary'
+              onClick={() => {
+                setPopoverShow(true)
+              }}>
+              {id ? '编辑' : '发布'}
+            </Button>
+          </Popover>
+        </div>
+        <div className='mr-[20px] cursor-pointer'>
+          <AvatarPopover />
+        </div>
+      </div>
       <div className='h-[100%]'>
-        <Editor valueChange={valueChange} />
+        <Editor
+          value={addArticleParams.content}
+          setValue={(value) => {
+            setAddArticleParams({
+              ...addArticleParams,
+              content: value
+            })
+          }}
+        />
       </div>
     </>
   )
