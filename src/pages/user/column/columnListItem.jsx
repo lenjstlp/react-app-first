dd58d6ee08dfe6fc8c827137d37662f4908027dc
lenjstlp <lenjstlp@gmail.com>
2025-05-18 02:29:27 +0800

    feat: 专栏查询&项目展示

---
+++ src/pages/user/column/columnListItem.jsx
@@ -0,0 +1,38 @@
+import { formatDate } from '@/utils'
+
+function ColumnListItem({ columnItem }) {
+  console.log(columnItem, '---')
+
+  return (
+    <div className='flex px-[15px] bg-[#fff] w-[100%] h-[100px] border-b-2 border-[#f7f8f9] py-[15px] cursor-pointer hover:bg-[#f5f5f5]'>
+      <div className='w-[100px] mr-[10px]'>
+        <img
+          className='w-[100%] h-[100%] object-cover'
+          src={columnItem.columnCover}
+          alt=''
+        />
+      </div>
+      <div className='flex flex-col justify-between'>
+        <div className='text-[20px]'>{columnItem.columnName}</div>
+        <div className='py-[2px] text-[#8a919f] truncate'>
+          {columnItem.columnIntro}
+        </div>
+        <div>
+          <div className='flex gap-[10px] text-[#8a919f]'>
+            <div>{formatDate(columnItem.createTime)}</div>
+            <div className='flex items-center'>
+              <div>文章数：</div>
+              {columnItem.articleNum}
+            </div>
+            <div className='flex items-center'>
+              <div>订阅数：</div>
+              {columnItem.subscriberNum}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ColumnListItem
--- src/pages/user/column/index.jsx
+++ src/pages/user/column/index.jsx
@@ -1,18 +1,33 @@
 import { useState, useEffect } from 'react'
 import Empty from './empty'
 import ColumnModal from './columnModal'
+import ColumnListItem from './columnListItem'
 
 import { getColumnList } from '@/apis/column'
 
 function Column() {
   // 进入该页面先查询专栏列表，为空显示创建专栏
   const [columnList, setColumnList] = useState([])
+
+  // 格式数据
+  function formatNumberData(str) {
+    if (!str) return 0
+    return str.split(',').length
+  }
   // 获取登陆人专栏列表
   async function queryColumnList() {
     const { code, data } = await getColumnList()
     if (code === 0) {
       console.log(data, '-----')
-      setColumnList(data)
+      setColumnList(
+        data.map((i) => {
+          return {
+            ...i,
+            articleNum: formatNumberData(i.articleIds),
+            subscriberNum: formatNumberData(i.subscriberIds)
+          }
+        })
+      )
     }
   }
   function refreshColumnList() {
@@ -24,10 +39,13 @@ function Column() {
   useEffect(() => {
     queryColumnList()
   }, [])
+
   return (
     <div>
       {columnList.length > 0 ? (
-        '专栏列表'
+        columnList.map((i) => {
+          return <ColumnListItem key={i.id} columnItem={i} />
+        })
       ) : (
         <Empty setModalOpen={setModalOpen} />
       )}
