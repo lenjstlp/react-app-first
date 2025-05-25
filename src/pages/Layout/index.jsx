da91e19d40c6f3ea27a6d182bd25724ee79213f1
lenjstlp <lenjstlp@gmail.com>
2025-05-25 20:43:57 +0800

    feat: 优化dict Context传入

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react'
+import { createContext, useMemo } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Layout } from 'antd'
 import TopNav from './TopNav'
@@ -9,8 +9,12 @@ const dictsContext = createContext()
 function ReactLayout() {
   const { dicts } = useDicts({ type: 'VIP_LEVEL,ARTICLE_CHANNEL' })
 
+  const context = useMemo(() => {
+    return { dicts }
+  }, [dicts])
+
   return (
-    <dictsContext.Provider value={{ dicts }}>
+    <dictsContext.Provider value={context}>
       <Layout className='h-[100%]'>
         <TopNav />
         <div className='h-[calc(100%-60px)] overflow-hidden'>
